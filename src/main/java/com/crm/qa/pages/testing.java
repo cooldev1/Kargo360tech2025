213ea8d5f728b82b674b4c05924cee5c85d906de
vishal.jain <vishal.jain@Cargo-IT001.FLYSPICE.COM>
2025-02-17 14:05:22 +0530

    updat testing page code

--- src/main/java/com/crm/qa/pages/testing.java
+++ src/main/java/com/crm/qa/pages/testing.java
@@ -3,5 +3,11 @@ package com.crm.qa.pages;
 import com.crm.qa.base.TestBase;
 
 public class testing extends TestBase{
-
+	
+	
+	//Actions:
+		public String validateLoginPageTitle(){
+			return driver.getTitle();
+		}
+	
 }
