d5164ec47fcc9aeac100ce99a6029dc2a0e88301
Devendra Singh1 <Devendra.singh1@FLYSPICE.COM>
2025-02-25 19:24:58 +0530

    broser comment

--- src/test/java/com/crm/qa/testcases/CartMasterListingPageTest.java
+++ src/test/java/com/crm/qa/testcases/CartMasterListingPageTest.java
@@ -426,11 +426,11 @@ public class CartMasterListingPageTest extends TestBase{
  
 	}	
 	
-// Closed the browser
-	@AfterMethod
-	public void tearDown(){
-		driver.quit();
-	}
+// commit browser Closed the browser
+//	@AfterMethod
+//	public void tearDown(){
+//		driver.quit();
+//	}
 	
 	
 	
